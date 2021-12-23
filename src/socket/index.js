69c00cb7796550d972f4e758de9f8c5a1ebedfa3
Beers15 <alexanderbeers15@gmail.com>
2021-12-23 13:01:11 -0800

    Fix merge conflict

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,16 +1,19 @@
+let users = new Map();
+
 module.exports = function (io) {
   io.on('connection', (socket) => {
     console.log(`Socket ${socket.id} connected`);
 
     socket.on('join', ({ room, username }) => {
       //ensures a single socket can only be in one room at a time
-      socket.rooms.forEach(room => {
+      socket.rooms.forEach((room) => {
         socket.leave(room);
       });
 
       try {
         socket.join(room);
         if (username) {
+          users.set(username, { socketId: socket.id, currentRoom: room });
           socket.username = username;
         }
         console.log(
@@ -29,6 +32,23 @@ module.exports = function (io) {
       } catch (err) {
         console.log(err);
       }
+    });
+
+    socket.on('get-users-for-room', ({ room }) => {
+      let usersInRoom = [...users].filter(
+        ([, value]) => value.currentRoom === room
+      );
+
+      console.log(room, ': Current Users ->', usersInRoom);
+      io.to(room).emit('get-users-for-room', { users: usersInRoom });
+    });
+
+    socket.on('disconnect', () => {
+      let leavingUser = [...users].find(
+        ([, value]) => value.socketId === socket.id
+      );
+      if(leavingUser)
+        users.delete(leavingUser[0]);
     });
   });
 };
