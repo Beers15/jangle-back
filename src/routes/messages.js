bc28356700e31aa1fa6594ede1dba69a574a53ca
Beers15 <alexanderbeers15@gmail.com>
2021-12-23 17:10:23 -0800

    Socket message event handler now handles adding an message to the Messages collection instead of a /messages POST route.

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -10,25 +10,6 @@ const getRoomMessages = async (req, res) => {
   res.status(200).json(allMessages);
 }
 
-const addMessage = async (req, res) => {
-  let msg = req.body;	
-  let current = new Date();
-  let date = `${current.getFullYear()}/${(current.getMonth() + 1)}/${current.getDate()}`;
-  let time = `${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
-  msg.timeSentFormatted =  `${date} ~ ${time}`;
-  msg.timeSent = current;
-
-  try {
-    let result = await Message.create(msg);
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}
-
-router.route('/')
-  .post(addMessage);
-
 router.route('/:room')
   .get(getRoomMessages);
 
