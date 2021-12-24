0d96a7e0eb769305b19bc59527cd8a3ef671ac95
Beers15 <alexanderbeers15@gmail.com>
2021-12-23 18:19:52 -0800

    Re-add createRoom event handler that can handle 1-1 rooms for /messages POST route.

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -10,7 +10,17 @@ const getAllRoom = async (req, res) => {
   res.status(200).json(allRoom);
 }
 
+
 const createRoom = async (req, res) => {
+  if(req.body.users) {
+    let check1 = await Room.find({ roomname: req.body.users[0] + '-' + req.body.users[1] });
+    let check2 = await Room.find({ roomname: req.body.users[1] + '-' + req.body.users[0] });
+    
+    if(check1.length > 0 || check2.length > 0) {
+      return res.status(409).json({"err": "You already have an open direct message room with this user."});
+    }
+  }
+
   try {
     let result = await Room.create(req.body);
     res.status(201).json(result);
