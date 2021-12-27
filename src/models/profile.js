806dbd25d347bdea3160c06cf0ea26ebf5029dfb
Beers15 <alexanderbeers15@gmail.com>
2021-12-27 12:09:04 -0800

    Update Profile model to handle image upload data. Add Multer middleware that uses cloudinary storage object to store the actual images remotely on POST and PUT /profile routes.

--- src/models/profile.js
+++ src/models/profile.js
@@ -2,12 +2,24 @@
 
 const mongoose = require('mongoose');
 
+const options = { toJSON: { virtuals: true } };
+
+const ImageSchema = new mongoose.Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const profileSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
     required: true,
   },
+  image: ImageSchema,
   preferredName: {
     type: String,
   },
@@ -18,7 +30,7 @@ const profileSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
-});
+}, options);
 
 const Profile = mongoose.model('Profile', profileSchema);
 
--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
-
+const { storage } = require('../utils/cloudinary');
+const upload = multer({ storage });
 const Profile = require('../models/profile');
 
 const getAllProfiles = async (req, res) => {
@@ -11,23 +13,43 @@ const getAllProfiles = async (req, res) => {
 }
 
 const getUserProfile = async (req, res) => {
-  console.log(req.params.user)
   let profile = await Profile.find({ username: req.params.user });
   res.status(200).json(profile);
 }
 
 const createProfile = async (req, res) => {
+  req.body.interests = req.body.interests.split(',');
+  if(req.file) {
+    req.body.image = { 
+      url: req.file.path,
+      filename: req.file.filename
+    }
+  }
+
   try {
     let result = await Profile.create(req.body);
     res.status(201).json(result);
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
 }
 
 const updateProfile = async (req, res) => {
-  let result = await Profile.findOneAndUpdate({ username: req.params.user }, req.body);
-  res.status(204).json(result);
+  req.body.interests = req.body.interests.split(',');
+  if(req.file) {
+    req.body.image = { 
+      url: req.file.path,
+      filename: req.file.filename
+    }
+  }
+  try {
+    let result = await Profile.findOneAndUpdate({ username: req.body.username }, req.body);
+    res.status(204).json(result);
+  } catch(err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
 }
 
 const deleteProfile = async (req, res) => {
@@ -37,11 +59,11 @@ const deleteProfile = async (req, res) => {
 
 router.route('/')
   .get(getAllProfiles)
-  .post(createProfile);
+  .post(upload.single('image'), createProfile);
 
 router.route('/:user')
   .get(getUserProfile)
-  .put(updateProfile)
+  .put(upload.single('image'), updateProfile)
   .delete(deleteProfile);
 
 
